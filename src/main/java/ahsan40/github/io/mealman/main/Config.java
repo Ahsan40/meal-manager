8b3d8f629a935abe7109cb174a2507d37dbb8874
Ahsan Khan <mkhan201122@bscse.uiu.ac.bd>
2022-09-29 13:05:23 +0600

    migrated to maven build system

---
+++ src/main/java/ahsan40/github/io/mealman/main/Config.java
@@ -0,0 +1,22 @@
+package ahsan40.github.io.mealman.main;
+
+public class Config {
+    // App Configs
+    public static String title = "Meal Manager";
+    public static String version = "v1.0";
+    public static String icon = "/img/icon3.png";
+    public static String css = "/css/styles.css";
+    public static double h = 487;
+    public static double w = 822;
+    public static String defaultTab = "dashboard";
+    public static String activeTabClass = "menu-item-active";
+
+    // Scenes
+    public static String mainScene = "/fxml/MainScene.fxml";
+    public static String dashboard = "/fxml/Dashboard.fxml";
+    public static String meal = "/fxml/Meal.fxml";
+    public static String bazzar = "/fxml/Bazzar.fxml";
+    public static String payments = "/fxml/Payments.fxml";
+    public static String people = "/fxml/People.fxml";
+    public static String extra = "/fxml/Extra.fxml";
+}
