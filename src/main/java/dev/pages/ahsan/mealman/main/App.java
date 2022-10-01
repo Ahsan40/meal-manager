2b6ab226c3de62b1f316251e766d2d73f72bad2b
Ahsan Khan <mkhan201122@bscse.uiu.ac.bd>
2022-10-01 21:07:10 +0600

    Added auto sharable  jar generator with maven

---
+++ src/main/java/dev/pages/ahsan/mealman/main/App.java
@@ -0,0 +1,9 @@
+package dev.pages.ahsan.mealman.main;
+
+import dev.pages.ahsan.mealman.main.Main;
+
+public class App {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
